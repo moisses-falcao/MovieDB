0de6acfa1831f96b2c44aa9a6dcd36cb9ad8b1a6
Moissés Vitor Rosa Falcão <CIANDT\moissesf@lnb035863spo.ciandt.global>
2022-05-03 15:35:16 -0300

    SplashActivity adicionada

--- CITMovieDatabase_MF/app/src/main/java/com/example/citmoviedatabase_mf/activities/SplashActivity.kt
+++ CITMovieDatabase_MF/app/src/main/java/com/example/citmoviedatabase_mf/activities/SplashActivity.kt
@@ -1,12 +1,39 @@
 package com.example.citmoviedatabase_mf.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.citmoviedatabase_mf.R
+import android.view.View
+import com.example.citmoviedatabase_mf.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySplashBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(binding.root)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+
+        supportActionBar!!.hide()
+
+        setupSplash()
+    }
+
+    private fun setupSplash() = with(binding){
+
+        ivLogoCIandT.alpha = 0f
+        ivLogoCIandT.animate().setDuration(1200).alpha(1f)
+
+        tvMovieCIandT.alpha = 0f
+        tvMovieCIandT.animate().setDuration(1200).alpha(1f).withEndAction {
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }
